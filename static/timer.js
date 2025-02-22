8ccdae398b8b0f15771ac6ad7a5a204440794506
ohataken <11092425+ohataken@users.noreply.github.com>
2025-02-23 02:02:39 +0900

    add Timer

---
+++ static/timer.js
@@ -0,0 +1,149 @@
+class Timer {
+
+  parseDuration(duration) {
+    return parseInt(duration) * 1000;
+  }
+
+  setDuration(duration) {
+    this.duration = this.parseDuration(duration);
+  }
+
+  getDuration() {
+    return this.duration;
+  }
+
+  serializeDuration() {
+    return this.duration / 1000;
+  }
+
+  getDurationMinutes() {
+    return Math.floor((this.duration || 0) / 1000 / 60);
+  }
+
+  getDurationSeconds() {
+    return Math.floor((this.duration || 0) / 1000) % 60;
+  }
+
+  getDurationSubseconds() {
+    return Math.floor((this.duration || 0) % 1000 / 10);
+  }
+
+  parseTargetTime(seconds) {
+    return new Date(seconds * 1000);
+  }
+
+  setTargetTime(seconds) {
+    this.targetTime = this.parseTargetTime(seconds);
+  }
+
+  getTargetTime() {
+    return this.targetTime;
+  }
+
+  serializeTargetTime() {
+    return this.targetTime.getTime() / 1000;
+  }
+
+  getTargetTimeBy(date) {
+    const ms = this.getDuration();
+    return new Date(date.getTime() + ms);
+  }
+
+  getRemainingTime(now) {
+    if (this.isRunning()) {
+      return this.targetTime - now;
+    }
+  }
+
+  isPassed(remainingTime) {
+    return remainingTime < 0;
+  }
+
+  getRemainingMinutes(remainingTime) {
+    if (this.isPassed(remainingTime)) {
+      return 0;
+    } else {
+      return Math.floor(remainingTime / 1000 / 60);
+    }
+  }
+
+  getRemainingSeconds(remainingTime) {
+    if (this.isPassed(remainingTime)) {
+      return 0;
+    } else {
+      return Math.floor(remainingTime / 1000) % 60;
+    }
+  }
+
+  getRemainingSubseconds(remainingTime) {
+    if (this.isPassed(remainingTime)) {
+      return 0;
+    } else {
+      return Math.floor((remainingTime % 1000) / 10);
+    }
+  }
+
+  getRemainingMinutesString(remainingTime) {
+    if (this.isStopped()) {
+      return this.getDurationMinutes().toString().padStart(2, '0');
+    } else if (!remainingTime || this.isPassed(remainingTime)) {
+      return '00';
+    } else {
+      const minutes = this.getRemainingMinutes(remainingTime);
+      return minutes.toString().padStart(2, '0');
+    }
+  }
+
+  getRemainingSecondsString(remainingTime) {
+    if (this.isStopped()) {
+      return this.getDurationSeconds().toString().padStart(2, '0');
+    } else if (!remainingTime || this.isPassed(remainingTime)) {
+      return '00';
+    } else {
+      const seconds = this.getRemainingSeconds(remainingTime);
+      return seconds.toString().padStart(2, '0');
+    }
+  }
+
+  getRemainingSubsecondsString(remainingTime) {
+    if (this.isStopped()) {
+      return this.getDurationSubseconds().toString().padStart(2, '0');
+    } else if (!remainingTime || this.isPassed(remainingTime)) {
+      return '00';
+    } else {
+      const subseconds = this.getRemainingSubseconds(remainingTime);
+      return subseconds.toString().padStart(2, '0');
+    }
+  }
+
+  setStatus(status) {
+    this.status = status;
+  }
+
+  getStatus() {
+    return this.getStatus;
+  }
+
+  serializeStatus() {
+    return this.status;
+  }
+
+  isRunning() {
+    return this.status === 'running';
+  }
+
+  isStopped() {
+    return this.status === 'stopped';
+  }
+
+  makeRunning(now) {
+    this.targetTime = this.getTargetTimeBy(now);
+    this.status = 'running';
+  }
+
+  makeStopped() {
+    this.startAt = null;
+    this.status = 'stopped';
+  }
+
+}
