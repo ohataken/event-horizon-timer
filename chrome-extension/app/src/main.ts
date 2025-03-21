b9ce7718d7e64c758f9e966732dfa9622fe80340
ohataken <11092425+ohataken@users.noreply.github.com>
2025-03-21 23:54:17 +0900

    use pinia

--- chrome-extension/app/src/main.ts
+++ chrome-extension/app/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
+import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
 
@@ -10,5 +11,7 @@ const router = createRouter({
 })
 
 const app = createApp(App)
+const pinia = createPinia()
 app.use(router)
+app.use(pinia)
 app.mount('#app')
