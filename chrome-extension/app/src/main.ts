566d3cfdc7bfd6e269448beadbdc0d606fa4f4d5
ohataken <11092425+ohataken@users.noreply.github.com>
2025-03-21 23:53:27 +0900

    use vue-router

--- chrome-extension/app/src/main.ts
+++ chrome-extension/app/src/main.ts
@@ -1,6 +1,14 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import './style.css'
 import App from './App.vue'
 
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: [
+  ],
+})
+
 const app = createApp(App)
+app.use(router)
 app.mount('#app')
