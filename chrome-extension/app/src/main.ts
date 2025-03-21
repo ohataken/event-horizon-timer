912522175fe05de5f08eb8bf5ecd13b9cc1030cd
ohataken <11092425+ohataken@users.noreply.github.com>
2025-03-22 00:10:30 +0900

    use vuetify

--- chrome-extension/app/src/main.ts
+++ chrome-extension/app/src/main.ts
@@ -10,8 +10,19 @@ const router = createRouter({
   ],
 })
 
+// Vuetify
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
 const app = createApp(App)
 const pinia = createPinia()
 app.use(router)
 app.use(pinia)
+app.use(vuetify)
 app.mount('#app')
