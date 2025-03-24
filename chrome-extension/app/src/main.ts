56d6e10edd84c934d4386cbfb92e5c9df1efd3e4
ohataken <11092425+ohataken@users.noreply.github.com>
2025-03-24 16:51:54 +0900

    fix: import 'vuetify/styles/main.css'

--- chrome-extension/app/src/main.ts
+++ chrome-extension/app/src/main.ts
@@ -12,6 +12,7 @@ const router = createRouter({
 })
 
 // Vuetify
+import 'vuetify/styles/main.css'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
