0347da48586bd1905114be2b6575c879ec5984f2
ohataken <11092425+ohataken@users.noreply.github.com>
2025-03-24 16:49:03 +0900

    import Material Design Icons

--- chrome-extension/app/src/main.ts
+++ chrome-extension/app/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
 import { createPinia } from 'pinia'
+import '@mdi/font/css/materialdesignicons.css'
 import './style.css'
 import App from './SidePanelApp.vue'
 
@@ -18,6 +19,9 @@ import * as directives from 'vuetify/directives'
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'mdi',
+  },  
 })
 
 const app = createApp(App)
