30bf2dfb6ba2df88bbfd2f5b9c7b4151b7cefaab
ohataken <11092425+ohataken@users.noreply.github.com>
2025-05-03 01:09:42 +0900

    feat: timer id を持っていなかったときに取得するボタンを表示するようにした。

--- chrome-extension/app/src/SidePanelApp.vue
+++ chrome-extension/app/src/SidePanelApp.vue
@@ -1,12 +1,17 @@
 <template>
   <v-app>
     <v-main>
-      <Timer />
+      <Timer v-if="timerIdStore.timerId" />
+      <CreateTimer v-else />
     </v-main>
   </v-app>
 </template>
 
 <script setup lang="ts">
+import { useTimerIdStore } from './stores/useTimerIdStore'
 import Timer from './components/Timer.vue'
+import CreateTimer from './components/CreateTimer.vue'
 
+const timerIdStore = useTimerIdStore()
+timerIdStore.fetchTimerId()
 </script>
