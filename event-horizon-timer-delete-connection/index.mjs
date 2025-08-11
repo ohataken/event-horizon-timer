a22c0caf773578b71b13c82ef2702bdb7808bb7b
ohataken <11092425+ohataken@users.noreply.github.com>
2025-08-12 00:57:50 +0900

    feat: event-horizon-timer-delete-connection

--- event-horizon-timer-delete-connection/index.mjs
+++ event-horizon-timer-delete-connection/index.mjs
@@ -1,2 +1,41 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, GetCommand, UpdateCommand, DeleteCommand } from "@aws-sdk/lib-dynamodb";
+
 export async function handler(event) { // eslint-disable-line no-unused-vars
+  const connectionId = event.requestContext.connectionId;
+  const tableName = process.env.TIMERS_TABLE_NAME; // eslint-disable-line no-undef
+  const connectionTableName = process.env.CONNECTIONS_TABLE_NAME; // eslint-disable-line no-undef
+  const dynamoDBClient = new DynamoDBClient({});
+  const dynamoDB = DynamoDBDocumentClient.from(dynamoDBClient);
+
+  try {
+    const connectionResult = await dynamoDB.send(new GetCommand({
+      TableName: connectionTableName,
+      Key: { id: connectionId },
+    }));
+
+    await dynamoDB.send(new UpdateCommand({
+      TableName: tableName,
+      Key: { id: connectionResult.Item.timer_id },
+      UpdateExpression: "DELETE client_ids :c",
+      ExpressionAttributeValues: {
+        ":c": new Set([connectionId])
+      }
+    }));
+
+    await dynamoDB.send(new DeleteCommand({
+      TableName: connectionTableName,
+      Key: { id: connectionId },
+    }));
+
+    return {
+      statusCode: 200,
+      body: "Connected",
+    };
+  } catch (error) { // eslint-disable-line no-unused-vars
+    return {
+      statusCode: 500,
+      body: "Failed to connect",
+    };
+  }
 }
