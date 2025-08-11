f4ee4d96e1e67a01d2400fb9d949500036eb7ab6
ohataken <11092425+ohataken@users.noreply.github.com>
2025-08-12 00:57:41 +0900

    feat: event-horizon-timer-create-timer

--- event-horizon-timer-create-timer/index.mjs
+++ event-horizon-timer-create-timer/index.mjs
@@ -1,2 +1,35 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+import crypto from 'crypto';
+
 export async function handler(event) { // eslint-disable-line no-unused-vars
+  const tableName = process.env.TIMERS_TABLE_NAME; // eslint-disable-line no-undef
+  const dynamoDBClient = new DynamoDBClient({});
+  const dynamoDB = DynamoDBDocumentClient.from(dynamoDBClient);
+
+  try {
+    const timerObject = {
+      id: crypto.randomUUID(),
+      client_ids: new Set(["sentinel"]),
+      status: "stopped",
+      target_time: 0,
+      duration: 0,
+      token: crypto.randomBytes(32).toString('hex'),
+    };
+
+    await dynamoDB.send(new PutCommand({
+      TableName: tableName,
+      Item: timerObject,
+    }));
+
+    return {
+      statusCode: 201,
+      body: JSON.stringify(timerObject),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: `Failed to create timer: ${error}` }),
+    };
+  }
 }
