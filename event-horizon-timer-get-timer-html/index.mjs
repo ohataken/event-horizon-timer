d5b37d36bb7fb518c87d8dab905122478f5f1953
ohataken <11092425+ohataken@users.noreply.github.com>
2025-08-12 01:00:03 +0900

    feat: event-horizon-timer-get-timer-html

--- event-horizon-timer-get-timer-html/index.mjs
+++ event-horizon-timer-get-timer-html/index.mjs
@@ -1,2 +1,19 @@
 export async function handler(event) { // eslint-disable-line no-unused-vars
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'text/html'
+    },
+    body: [
+      '<!DOCTYPE html>',
+      '<html lang="en">',
+      '<head>',
+      '  <meta charset="UTF-8">',
+      '  <title>Event Horizon Timer</title>',
+      '</head>',
+      '<body>',
+      '</body>',
+      '</html>'
+    ].join('\n')
+  };
 }
