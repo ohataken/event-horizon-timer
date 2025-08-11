ed98deaa005ccfef3ea130028baa6ddd660d5bd8
ohataken <11092425+ohataken@users.noreply.github.com>
2025-08-12 00:57:22 +0900

    feat: event-horizon-timer-create-connection

--- event-horizon-timer-create-connection/index.mjs
+++ event-horizon-timer-create-connection/index.mjs
@@ -1,2 +1,38 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, UpdateCommand } from "@aws-sdk/lib-dynamodb";
+
 export async function handler(event) { // eslint-disable-line no-unused-vars
+  const id = event.queryStringParameters?.timer_id;
+  const connectionId = event.requestContext.connectionId;
+  const tableName = process.env.TIMERS_TABLE_NAME; // eslint-disable-line no-undef
+  const dynamoDBClient = new DynamoDBClient({});
+  const dynamoDB = DynamoDBDocumentClient.from(dynamoDBClient);
+
+  if (!id) {
+    return {
+      statusCode: 400,
+      body: "Missing timer_id",
+    };
+  }
+
+  try {
+    await dynamoDB.send(new UpdateCommand({
+      TableName: tableName,
+      Key: { id: id },
+      UpdateExpression: "ADD client_ids :c",
+      ExpressionAttributeValues: {
+        ":c": new Set([connectionId])
+      }
+    }));
+
+    return {
+      statusCode: 200,
+      body: "Connected"
+    };
+  } catch (error) { // eslint-disable-line no-unused-vars
+    return {
+      statusCode: 500,
+      body: "Failed to connect"
+    };
+  }
 }
