a5ab7f506d0b3f8bb1dfcce192649da110352af2
ohataken <11092425+ohataken@users.noreply.github.com>
2025-08-17 01:03:57 +0900

    feat: load Font Awesome

--- event-horizon-timer-get-timer-html/index.mjs
+++ event-horizon-timer-get-timer-html/index.mjs
@@ -11,6 +11,7 @@ export async function handler(event) { // eslint-disable-line no-unused-vars
       '  <meta charset="UTF-8">',
       '  <meta name="viewport" content="width=device-width, initial-scale=1.0">',
       '  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">',
+      '  <script src="https://kit.fontawesome.com/6fdd8a7bbb.js" crossorigin="anonymous"></script>',
       '  <title>Event Horizon Timer</title>',
       '</head>',
       '<body>',
