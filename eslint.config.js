7e4f675a7a79cd2fe612e41a8926e43681c67b23
ohataken <11092425+ohataken@users.noreply.github.com>
2025-09-12 22:30:42 +0900

    chore: describe directive comments

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from "eslint/config";
-import eslintConfigESLint from "eslint-config-eslint";
+import { defineConfig } from "eslint/config"; // eslint-disable-line n/no-unpublished-import -- eslint/config
+import eslintConfigESLint from "eslint-config-eslint"; // eslint-disable-line n/no-unpublished-import -- eslint-config-eslint
 
 export default defineConfig([eslintConfigESLint]);
